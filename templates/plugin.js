17bdc8fe09b11d047bc8d19233d5a09d80e20662
Oleg Bochagov <w.iohansson@gmail.com>
2021-12-25 18:58:28 +0100

    fix: inject OneSignal after sdk load

--- templates/plugin.js
+++ templates/plugin.js
@@ -1,13 +1,12 @@
-window.$OneSignal = window.OneSignal = window.OneSignal || [];
-
-OneSignal.push(() => {
-  OneSignal.SERVICE_WORKER_PARAM = <%= JSON.stringify(options.swParams) %>
-  OneSignal.SERVICE_WORKER_PATH = <%= JSON.stringify(options.workerPath) %>
-  OneSignal.SERVICE_WORKER_UPDATER_PATH = <%= JSON.stringify(options.updaterPath) %>
-  OneSignal.init(<%= JSON.stringify(options.init, null, 2) %>)
-  window.$OneSignal = window.OneSignal;
-})
-
 export default function (ctx, inject) {
-  inject('OneSignal', OneSignal)
+  window.$OneSignal = window.OneSignal = window.OneSignal || [];
+
+  OneSignal.push(() => {
+    OneSignal.SERVICE_WORKER_PARAM = <%= JSON.stringify(options.swParams) %>
+    OneSignal.SERVICE_WORKER_PATH = <%= JSON.stringify(options.workerPath) %>
+    OneSignal.SERVICE_WORKER_UPDATER_PATH = <%= JSON.stringify(options.updaterPath) %>
+    OneSignal.init(<%= JSON.stringify(options.init, null, 2) %>)
+    window.$OneSignal = window.OneSignal;
+    inject('OneSignal', window.OneSignal)
+  });
 }
